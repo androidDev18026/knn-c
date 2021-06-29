7ad2f9af2c38651bf6ff6fa69e9e8b8742e5963e
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-06-29 21:52:57 +0300

    Update euclidean_dist.c

--- euclidean_dist.c
+++ euclidean_dist.c
@@ -1,7 +1,11 @@
-/* Reads pairs of (x,y) points from file and calculates the euclidean distance between the points 
- *
+/* 
+ * Reads pairs of (x,y) points from file and calculates the euclidean distance
+ * between pairs of point and a boolean adjecency matrix with the points that
+ * are close to each other represented with 1, otherwise 0. The similarity of 
+ * 2 points is defined by a threashold provided by the user.
+ *  
  * Compile : gcc -Wall -std=c99 -g -O3 -lm -o out euclidean_dist.c                 
- * Add < -D _DEBUG > for verbose printing of the points,                   
+ * Add < -D _DEBUG > argument for verbose printing of the points,                   
  * the distance and the adjacency matrix.                   
  */
 
@@ -9,7 +13,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <assert.h>
 #include <sys/time.h>
 #include <time.h>
@@ -25,7 +28,7 @@ struct Point
 };
 
 // Random initialization of the points - debugging
-void init(struct Point* p, const unsigned int size)
+void init(struct Point* p, unsigned const int size)
 {
   assert(size > 0);
 	
@@ -81,17 +84,50 @@ float* dist(struct Point p, struct Point* rest, unsigned const int size)
   return euclidean_dist;
 }
 
-void show_points(struct Point* points, unsigned const int size)
+extern inline void show_points(struct Point* points, unsigned const int size)
 {
   puts("===Points===");
   for (unsigned int i = 0; i < size; ++i) {
-     printf("%d. (%d,%d)\n", (i+1), *&(points+i)->x, *&(points+i)->y);
+     printf("%-d. (%d,%d)\n", (i+1), *&(points+i)->x, *&(points+i)->y);
   }
+}	
+
+extern inline void show_dist_matrix(float** euclidean_dist, unsigned const int size)
+{
+  puts("============Distance Matrix============");
+  for (unsigned int i = 0; i < size; ++i) {
+    for (unsigned int j = 0; j < size; ++j) {
+      printf("%-7.2f", *(*(euclidean_dist+i) + j));
+    }
+    puts("");
+  } 
+}	
+
+static void show_adj_matrix(int** similar, unsigned const int size)
+{
+	puts("===========Adjacency Matrix============");
+  printf("%-3s", "   ");
+  for (unsigned int i = 0; i < size; ++i) {
+    printf("%-3d", (i+1));
+  }
+  puts("");
+  printf("%-3s", "   ");
+  for (unsigned int i = 0; i < size; ++i) {
+    printf("---");
+  }
+  puts("");
+  for (unsigned int i = 0; i < size; ++i) {
+    printf("%-2d|", (i+1));
+    for (unsigned int j = 0; j < size; ++j) {
+      printf("%-3d", *(*(similar+i) + j));
+    }
+    puts("");
+  } 
+  puts("");
 }
 
-unsigned long get_num_lines(const char* filename)
+unsigned long get_num_points(const char* filename)
 {
-
   FILE* fp = fopen(filename, "r");
   char ch;
   unsigned long nlines = 0;
@@ -104,7 +140,9 @@ unsigned long get_num_lines(const char* filename)
   }
 	
   printf("Read %ld pairs of points from file %s.\n", nlines, filename);
-	 
+	
+	fclose(fp);
+	
   return nlines;
 }
 
@@ -133,10 +171,11 @@ int main(int argc, const char** argv)
   
   const char* filename = *(argv+1);
   const float th = atof(*(argv+2)) > 0 ? atof(*(argv+2)) : rand() % 100; 
-  unsigned const long SIZE = get_num_lines(filename);
+  unsigned const long SIZE = get_num_points(filename);
   static struct timeval start, end;
+  struct Point* points;
   
-  struct Point* points = read_p_from_file(filename, SIZE);
+  points = read_p_from_file(filename, SIZE);
   // init(points, SIZE);
 	
   printf("Finding pairs with distance less than %.2f\n", th);
@@ -146,7 +185,7 @@ int main(int argc, const char** argv)
     exit(-1);
   }
   
-  int** similar = malloc(SIZE * sizeof(int*));
+  int** similar = malloc(SIZE*sizeof(int*));
   for (unsigned int i = 0; i < SIZE; ++i) {
     *(similar+i) = malloc(SIZE*sizeof(int));
   }
@@ -160,6 +199,7 @@ int main(int argc, const char** argv)
   for (unsigned int i = 0; i < SIZE; ++i) {
     *(euclidean_dist+i) = dist(*(points+i), points, SIZE);
   }
+  puts("Calculated the distance between all points.");
   
   find_similar(points, euclidean_dist, SIZE, th, similar);
   
@@ -167,42 +207,16 @@ int main(int argc, const char** argv)
    
   #ifdef _DEBUG
   show_points(points, SIZE);
-  
-  puts("============Distance Matrix============");
-  for (unsigned int i = 0; i < SIZE; ++i) {
-    for (unsigned int j = 0; j < SIZE; ++j) {
-      printf("%-7.2f", *(*(euclidean_dist+i) + j));
-    }
-    puts("");
-  } 
-  puts("===========Adjacency Matrix============");
-  printf("%-3s", "   ");
-  for (unsigned int i = 0; i < SIZE; ++i) {
-    printf("%-3d", (i+1));
-  }
-  puts("");
-  printf("%-3s", "   ");
-  for (unsigned int i = 0; i < SIZE; ++i) {
-    printf("---");
-  }
-  puts("");
-  for (unsigned int i = 0; i < SIZE; ++i) {
-    printf("%-2d|", (i+1));
-    for (unsigned int j = 0; j < SIZE; ++j) {
-      printf("%-3d", *(*(similar+i) + j));
-    }
-    puts("");
-  } 
-  
+  show_dist_matrix(euclidean_dist, SIZE);
+	show_adj_matrix(similar, SIZE);  
   #endif
   
   #ifndef _DEBUG
   float elapsed =  (float) (end.tv_usec - start.tv_usec) / pow(10,6) +
                                     (float) (end.tv_sec - start.tv_sec);
   
-  puts("=====================");
-  printf("Time elapsed %.3fs\n", elapsed);
-  puts("=====================");
+  printf("\nTime elapsed(s) : %.3f\n", elapsed);
+  puts("==========================");
   #endif
   
   free(points);
